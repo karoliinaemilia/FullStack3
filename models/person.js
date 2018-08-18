7126ae6b6a79e6d0004a6b380f34c9e4406267cd
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-18 20:14:57 +0300

    3.14

--- models/person.js
+++ models/person.js
@@ -4,9 +4,20 @@ const url = 'mongodb://<dbuser>:<dbpassword>@ds125322.mlab.com:25322/puhelinluet
 
 mongoose.connect(url)
 
-const Person = mongoose.model('Person', {
+const personSchema = new mongoose.Schema({
   name: String,
   number: String,
+  id: String
 })
+
+personSchema.statics.format = function(person) {
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  }
+}
+
+const Person = mongoose.model('Person', personSchema)
 
 module.exports = Person
