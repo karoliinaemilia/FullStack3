cf52a13bbafc24596eb3cb4e018dfa65fa1885ac
Karoliina Koivu <karoliinakoivu@gmail.com>
2018-08-18 13:40:33 +0300

    3.2

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ let persons = [
   }
 ]
 
+app.get('/info', (req, res) => {
+  const date = new Date()
+  res.send(`<p>puhelinluettelossa ${persons.length} henkilön tiedot</p>
+  <p>${date}</p>`)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
